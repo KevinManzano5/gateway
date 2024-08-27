83a2c6277d932fac1930cf0a97fd41ffda47e897
Kevin Alejandro Manzano Torres <kevinalejandromanzanotorres@MacBook-Pro-de-Kevin.local>
2024-08-27 08:39:55 -0500

    feat: :sparkles: add register user dto

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,15 @@
-import { Controller, Inject, Post } from '@nestjs/common';
+import { Body, Controller, Inject, Post } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 
 import { NATS_SERVICE } from '../config/constants/services';
+import { RegisterUserDto } from './dto/register-user.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
 
   @Post('register')
-  registerUser() {
-    return this.client.send('auth.register.user', {});
+  registerUser(@Body() registerUserDto: RegisterUserDto) {
+    return this.client.send('auth.register.user', registerUserDto);
   }
 }
